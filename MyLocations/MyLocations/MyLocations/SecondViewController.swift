d685aa7b001fdd9f0396269b7bd589b75179aef1
Jesse Pruitt <AverageEar@Sarah-Marshalls-MacBook-Pro.local>
2015-05-12 15:52:15 -0500

    Adding MyLocations

---
+++ MyLocations/MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Terry Griffin on 3/24/15.
+//  Copyright (c) 2015 mwsu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 1
+        let myLocation = (self.tabBarController as CustomTabBarController).currentPoint
+        let location = CLLocationCoordinate2D(
+            latitude: myLocation.lat,
+            longitude: -myLocation.lon
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        mapView.addAnnotation(annotation)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
